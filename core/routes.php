f0a571c97968546f55da6f5e9cc84a7474c62864
Bokun89 <r.a.denekamp@gmail.com>
2022-04-20 16:25:56 +0200

    home page and stuff

---
+++ core/routes.php
@@ -0,0 +1,23 @@
+<?php
+//
+//// Negeer dit allemaal en volg even Stephan zn vids weer
+//
+//// Routing zoals Maurice gedaan heeft
+//if(true == !empty($_POST)) :
+//    switch($request)
+//    {
+//        //case 'random post ding hier' :
+//        //Router::post(controller 'somethingsomething', method 'iets')
+//        //break;
+//
+//        default :
+//            view('404');
+//            break;
+//    }
+//    else :
+//        switch ($request)
+//        {
+//            case '/' :
+//            case '/home' :
+//                Router::get(contro)
+//        }
