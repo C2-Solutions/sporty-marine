424722b09e8d3894336a77eb9a91056202002f08
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-04-21 13:13:21 +0200

    Add CSS, other minor updates
    Entirely written by @boyo785, just pushed by me.
    
    Co-Authored-By: Enno <32169841+boyo785@users.noreply.github.com>

--- views/shared/header.php
+++ views/shared/header.php
@@ -5,6 +5,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" href="public/css/mystylesheet.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="../public/css/mystylesheet.css" rel="stylesheet">
 
     <title>Sporty-marine</title>
 </head>
@@ -13,14 +14,14 @@
 
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: powderblue">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Sporty-marine</a>
+        <a class="navbar-brand" href="">Sporty-marine</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link aria-current="page" href="home">Home</a>
+                    <a class="nav-link aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link"  href="modellen">Modellen</a>
