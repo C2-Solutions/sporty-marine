7193681a04e23d1d9683826395596383e3339b61
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-17 12:54:26 +0200

    Fix admin ,use the proper session keywords

--- App/Controllers/AuthenticationController.php
+++ App/Controllers/AuthenticationController.php
@@ -44,11 +44,7 @@ class AuthenticationController
             $logged = $stmt->fetch();
 
             if ($logged) {
-                $stmt->bindparam($logged['id'], $password);
-                $stmt->fetch();
-
-                $hashPwd = self::hashPassword($password);
-                if (password_verify($_POST['password'], $hashPwd)) {
+                if (password_verify($_POST['password'], $logged['password'])) {
                     session_regenerate_id();
                     $_SESSION['loggedin'] = true;
                     $_SESSION['username'] = $_POST['username'];
