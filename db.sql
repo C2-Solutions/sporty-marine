a985eef45992896140c1e52bb3a155c0a01675cb
kyrauwu <bryan-bao@hotmail.com>
2022-06-17 13:30:13 +0200

    updated README and db.sql

--- db.sql
+++ db.sql
@@ -48,14 +48,17 @@ CREATE TABLE IF NOT EXISTS contact_inquiries
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-create table IF NOT EXISTS images
-(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    modelid INT NOT NULL,
-    image VARCHAR(255) NOT NULL,
-    CONSTRAINT `model_ids` FOREIGN KEY(modelid) REFERENCES models(id)
-);
+INSERT INTO users (username, password)
+VALUES ('admin', 'password123');
 
--- Create admin user. MAKE SURE TO CHANGE THIS TO SOMETHING SECURE
-/* INSERT INTO users (username, password)
-VALUES ('admin', 'password123'); */
+INSERT INTO flevonautica.boattype (type) VALUES ('Speedboat');
+INSERT INTO flevonautica.boattype (type) VALUES ('Flexboat');
+INSERT INTO flevonautica.boattype (type) VALUES ('Sailboat');
+
+INSERT INTO flevonautica.models (id, boattype, name, price, description, image, lgth, width, weight, airdraft, draft, maxpk, maxpers, builtin, cec, status, availability) VALUES (1, 1, 'Sporty 15', 15000, 'Dit is een van onze topboten!
+
+              Kwaliteit en klasse krijg je zelden voor deze goedkope prijs!', 'testboot.jpg', 15.00, 3.00, 300.00, 50, 20.00, 50, 5, 2022, 'A', 'Nieuw', 1)
+
+INSERT INTO flevonautica.contact_inquiries (name, email, phone, message, created_at) VALUES ('Jan Jansen', 'Jan.Jansen@gmail.com', '068881459', 'Ik wil graag een boot bekijken? Kan dat? Mail me terug a.u.b.', DEFAULT)
+
+INSERT INTO flevonautica.contact_inquiries (name, email, phone, message, created_at) VALUES ('Piet Pieterszoon', 'PP36@hotmail.be', '061515134', 'Erg geïnteresseerd! Je kan me bellen!', DEFAULT)
