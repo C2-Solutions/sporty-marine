9cca9128be1e9eb85ce0198668ca8c65106c5880
kyrauwu <bryan-bao@hotmail.com>
2022-06-17 13:34:14 +0200

    Update dashboard.view.php

--- views/admin/dashboard.view.php
+++ views/admin/dashboard.view.php
@@ -1,6 +1,6 @@
-<!--SCUMMY SHIT!! REMOVE THIS AFTER LOGIN IS DONE-->
-
-<?php $_SESSION['adminid']     =   (!empty($admin['id'])) ? $admin['id'] : 'a';
-$_SESSION['username']    =   (!empty($admin['username'])) ? $admin['username'] : 'a';
-?>
 <h1>U bent nu ingelogd als administrator. Welkom <?php echo $_SESSION['username']?>.</h1>
+<br>
+<br>
+<br>
+<br>
+<br>
